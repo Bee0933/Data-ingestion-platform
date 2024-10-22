8154ec453b11b920fdb2e8f802859cfdd28d9ad4
Bee0933 <bestnyah7@gmail.com>
2024-10-23 00:42:33 +0100

    fix: ansible boostrap playbooks
    Signed-off-by: Bee0933 <bestnyah7@gmail.com>

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,62 +1,62 @@
 # This is to set all network configs for the SFTP platform 
 
 # source server firewall configs 
-# resource "digitalocean_firewall" "src-server-fw" {
-#   name = "source-server-firewall"
+resource "digitalocean_firewall" "src-server-fw" {
+  name = "source-server-firewall"
 
-#   droplet_ids = [digitalocean_droplet.src-server-0.id]
+  droplet_ids = [digitalocean_droplet.src-server-0.id]
 
-#   inbound_rule {
-#     protocol         = "tcp"
-#     port_range       = "22"
-#     source_addresses = ["0.0.0.0/0", "::/0"]
-#   }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
 
-#   inbound_rule {
-#     protocol         = "tcp"
-#     port_range       = "80"
-#     source_addresses = ["0.0.0.0/0", "::/0"]
-#   }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
 
-#   inbound_rule {
-#     protocol         = "tcp"
-#     port_range       = "443"
-#     source_addresses = ["0.0.0.0/0", "::/0"]
-#   }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
 
-#   inbound_rule {
-#     protocol         = "icmp"
-#     source_addresses = ["0.0.0.0/0", "::/0"]
-#   }
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
 
-#   outbound_rule {
-#     protocol              = "tcp"
-#     port_range            = "53"
-#     destination_addresses = ["0.0.0.0/0", "::/0"]
-#   }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
 
-#   outbound_rule {
-#     protocol              = "udp"
-#     port_range            = "53"
-#     destination_addresses = ["0.0.0.0/0", "::/0"]
-#   }
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
 
-#   outbound_rule {
-#     protocol              = "icmp"
-#     destination_addresses = ["0.0.0.0/0", "::/0"]
-#   }
-#   outbound_rule {
-#     protocol              = "tcp"
-#     port_range            = "80"
-#     destination_addresses = ["0.0.0.0/0", "::/0"]
-#   }
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "80"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
 
-#   outbound_rule {
-#     protocol              = "tcp"
-#     port_range            = "443"
-#     destination_addresses = ["0.0.0.0/0", "::/0"]
-#   }
-# }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "443"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
 
 # # Airbyte Server firewall configs
 # resource "digitalocean_firewall" "airbyte-server-fw" {
